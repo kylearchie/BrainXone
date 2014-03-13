0a5e1e738909242b561338991244feef9ee876ba
Graham Roth <gsroth@stanford.edu>
2014-03-13 16:53:17 -0700

    You can create quizzes now, barely.

--- BrainXone/src/brainxone/UpdatePrivacyServlet.java
+++ BrainXone/src/brainxone/UpdatePrivacyServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.mysql.jdbc.Statement;
+import java.sql.Statement;
 
 /**
  * Servlet implementation class UpdatePrivacyServlet
